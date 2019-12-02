5f7df6c41fabc66e2fddce67e4199ff90ea1fb1e
neogeoss <neogeo-s@hanmail.net>
2019-12-02 17:35:49 +0900

    rotateRR, LL, LR, RL, and get balance, insertNode, display functions are implemented.

--- main.c
+++ main.c
@@ -53,3 +53,117 @@ Node* rotateRR(Node* node)
 	return rightChild;
 }
 
+Node* rotateLR(Node* node)
+{
+	Node* leftChild = node->leftChild;
+	node->leftChild = rotateRR(leftChild);
+	setHeight(node->leftChild);
+	return rotateLL(node);
+}
+
+Node* rotateRL(Node* node)
+{
+	Node* rightChild = node->rightChild;
+	node->rightChild = rotateRR(rightChild);
+	setHeight(node->rightChild);
+	return rotateLL(node);
+}
+
+Node* balance(Node* node) 
+{
+	int difference = getDifference(node);
+	if (difference >= 2)
+	{
+		if (getDifference(node->leftChild) >= 1)
+		{
+			node = rotateLL(node);
+		}
+		else
+		{
+			node = rotateLR(node);
+		}
+
+	}
+	else if (difference <= 2)
+	{
+		if (getDifference(node->rightChild) <= -1)
+		{
+			node = rotateRR(node);
+		}
+		else
+		{
+			node = rotateRL(node);
+		}
+	}
+	setHeight(node);
+	return node;
+
+}
+
+Node* insertNode(Node* node, int data)
+{
+	if (!node)
+	{
+		node = (Node*)malloc(sizeof(Node));
+		node->data = data;
+		node->height = 1;
+		node->leftChild = node->rightChild = NULL;
+	}
+	else if (data < node->data)
+	{
+		node->leftChild = insertNode(node->leftChild, data);
+		node = balance(node);
+	}
+	else if (data > node->data)
+	{
+		node->rightChild = insertNode(node->rightChild, data);
+		node = balance(node);
+	}
+	else
+	{
+		printf("데이터 중복이 발생했습니다.\n");
+	}
+	return node;
+}
+
+Node* root = NULL;
+void display(Node* node, int level)
+{
+	if (node != NULL)
+	{
+		display(node->rightChild, level + 1);
+		printf("\n");
+		if (node == root)
+		{
+			printf("Root: ");
+		}
+		for (int i = 0; i < level && node != root; i++)
+		{
+			printf("     ");
+		}
+		printf("%d(%d)", node->data, getHeight(node));
+		display(node->leftChild, level + 1);
+	}
+}
+
+int main(void)
+{
+	root = insertNode(root, 7);
+	root = insertNode(root, 6);
+	root = insertNode(root, 5);
+	root = insertNode(root, 3);
+	root = insertNode(root, 1);
+	root = insertNode(root, 9);
+	root = insertNode(root, 8);
+	root = insertNode(root, 12);
+	root = insertNode(root, 16);
+	root = insertNode(root, 18);
+	root = insertNode(root, 23);
+	root = insertNode(root, 21);
+	root = insertNode(root, 14);
+	root = insertNode(root, 15);
+	root = insertNode(root, 19);
+	root = insertNode(root, 20);
+	display(root, 1); printf("\n");
+	system("pause");
+}
